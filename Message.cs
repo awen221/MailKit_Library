c8c14316f87b77927711ee456ca627cbfb9959ca
Larry.Lai <Larry.Lai@portwell.com.tw>
2023-02-18 15:37:42 +0800

    first commit

---
+++ Message.cs
@@ -0,0 +1,56 @@
+﻿using MimeKit;
+
+namespace MailKit_Library
+{
+    public class Message
+    {
+        static MimeMessage Get(string senderName, string senderAddress, string subject,
+            string[] to, string[]? cc, string[]? bcc, MimeEntity body)
+        {
+            MimeMessage message = new();
+            message.Sender = new MailboxAddress(senderName, senderAddress);
+            message.Subject = subject;
+            foreach (var _to in to) message.To.Add(MailboxAddress.Parse(_to));
+            if (cc is not null)
+                foreach (var _cc in cc) message.Cc.Add(MailboxAddress.Parse(_cc));
+            if (bcc is not null)
+                foreach (var _bcc in bcc) message.Bcc.Add(MailboxAddress.Parse(_bcc));
+
+            message.Body = body;
+
+            return message;
+        }
+
+        static public MimeMessage GetText(string senderName, string senderAddress, string subject,
+            string[] to, string[]? cc, string[]? bcc, string text)
+        {
+            return Get(senderName, senderAddress, subject, to, cc, bcc, Body.GetText(text));
+        }
+        static public MimeMessage GetHtml(string senderName, string senderAddress, string subject,
+            string[] to, string[]? cc, string[]? bcc, string html)
+        {
+            return Get(senderName, senderAddress, subject, to, cc, bcc, Body.GetHtml(html));
+        }
+
+        class Body
+        {
+            static public MimeEntity GetText(string text)
+            {
+
+                var builder = new BodyBuilder();
+                builder.TextBody = text;
+
+                return builder.ToMessageBody();
+            }
+
+            static public MimeEntity GetHtml(string text)
+            {
+
+                var builder = new BodyBuilder();
+                builder.HtmlBody = text;
+
+                return builder.ToMessageBody();
+            }
+        }
+    }
+}
---
+++ Smtp.cs
@@ -0,0 +1,46 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+
+
+namespace MailKit_Library
+{
+    public class Smtp
+    {
+        static void Send(
+            string host, int port, string username, string password,
+            MimeMessage message
+            )
+        {
+            using (SmtpClient client = new())
+            {
+                client.Connect(host, port, SecureSocketOptions.SslOnConnect);
+                client.Authenticate(username, password);
+
+                client.Send(message);
+
+                client.Disconnect(true);
+            }
+        }
+
+        static public void SendText(string host, int port, string username, string password,
+            string sendername, string senderaddress, string subject, string[] to, string[]? cc, string[]? bcc,
+            string text)
+        {
+            using (MimeMessage message = Message.GetText(sendername, senderaddress, subject, to, cc, bcc, text))
+            {
+                Send(host, port, username, password, message);
+            }
+        }
+
+        static public void SendHtml(string host, int port, string username, string password,
+            string sendername, string senderaddress, string subject, string[] to, string[]? cc, string[]? bcc,
+            string html)
+        {
+            using (MimeMessage message = Message.GetHtml(sendername, senderaddress, subject, to, cc, bcc, html))
+            {
+                Send(host, port, username, password, message);
+            }
+        }
+    }
+}
