d9eb978156901ae3a7d5dc3d7d0362233ea139c2
Larry.Lai <Larry.Lai@portwell.com.tw>
2023-06-08 16:03:51 +0800

    optimization

--- Smtp.cs
+++ Smtp.cs
@@ -8,35 +8,29 @@ namespace MailKit_Library
     {
         static void Send(SecureSocketOptions secure, string host, int port, string username, string password, MimeMessage message)
         {
-            using (SmtpClient client = new())
-            {
-                client.Connect(host, port, secure);
-                client.Authenticate(username, password);
+            using SmtpClient client = new();
+            client.Connect(host, port, secure);
+            client.Authenticate(username, password);
 
-                client.Send(message);
+            client.Send(message);
 
-                client.Disconnect(true);
-            }
+            client.Disconnect(true);
         }
 
         static public void SendText(SecureSocketOptions secure, string host, int port, string username, string password,
             string sendername, string senderaddress, string subject, string[] to, string[]? cc, string[]? bcc,
             string textbody)
         {
-            using (MimeMessage message = Message.GetText(sendername, senderaddress, subject, to, cc, bcc, textbody))
-            {
-                Send(secure, host, port, username, password, message);
-            }
+            using MimeMessage message = Message.GetText(sendername, senderaddress, subject, to, cc, bcc, textbody);
+            Send(secure, host, port, username, password, message);
         }
 
         static public void SendHtml(SecureSocketOptions secure, string host, int port, string username, string password,
             string sendername, string senderaddress, string subject, string[] to, string[]? cc, string[]? bcc,
             string htmlbody)
         {
-            using (MimeMessage message = Message.GetHtml(sendername, senderaddress, subject, to, cc, bcc, htmlbody))
-            {
-                Send(secure, host, port, username, password, message);
-            }
+            using MimeMessage message = Message.GetHtml(sendername, senderaddress, subject, to, cc, bcc, htmlbody);
+            Send(secure, host, port, username, password, message);
         }
     }
 }
